00f81fc077b54c8a6edc4065a7d0c6985e911126
Supriyo <supriyomalstartup@gmail.com>
2026-05-30 18:23:20 +0530

    feat(server): add routes for user by username and org members lookup

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,12 +1,20 @@
 import express from "express";
 import { getMe } from "../controllers/authController.js";
 import { protect } from "../middleware/authMiddleware.js";
-import { updateUserProfile } from "../controllers/userController.js";
+import {
+    updateUserProfile,
+    getUserByUsername,
+    getUsersByCollege,
+    getUsersByCompany,
+} from "../controllers/userController.js";
 const userRouter = express.Router();
 
 
 userRouter.get("/profile", protect, getMe)
 userRouter.put("/profile", protect, updateUserProfile)
+userRouter.get("/college/:name/members", protect, getUsersByCollege)
+userRouter.get("/company/:name/members", protect, getUsersByCompany)
+userRouter.get("/:username", protect, getUserByUsername)
 
 
 export default userRouter;
