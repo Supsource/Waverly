5a01e343f571e3581d35b4c5c79a0e2d41433b99
Supriyo <supriyomalstartup@gmail.com>
2026-05-30 18:23:20 +0530

    feat(server): support companyName in profile update and add user lookup and org member endpoints

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -2,7 +2,7 @@ import User from "../models/User.js";
 
 export const updateUserProfile = async (req, res) => {
     try {
-        const { name, additionalName, bio, collegeName, startYear, endYear, profilePic } = req.body;
+        const { name, additionalName, bio, collegeName, companyName, startYear, endYear, profilePic } = req.body;
 
         if (name !== undefined && !name.trim()) {
             return res.status(400).json({ message: "Name cannot be empty" })
@@ -24,7 +24,8 @@ export const updateUserProfile = async (req, res) => {
         if (additionalName !== undefined) user.additionalName = additionalName;
         if (bio !== undefined) user.bio = bio;
         if (collegeName !== undefined) user.collegeName = collegeName;
-        
+        if (companyName !== undefined) user.companyName = companyName;
+
         if (startYear !== undefined) {
             user.startYear = startYear === "" ? 0 : Number(startYear);
         }
@@ -46,3 +47,57 @@ export const updateUserProfile = async (req, res) => {
         res.status(500).json({ message: "Internal server error" })
     }
 }
+
+export const getUserByUsername = async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.params.username }).select("-password");
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const isOwnProfile = req.user._id.toString() === user._id.toString();
+        const userObj = user.toJSON();
+
+        if (!isOwnProfile) {
+            delete userObj.email;
+        }
+
+        res.status(200).json({ user: userObj, isOwnProfile });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const getUsersByCollege = async (req, res) => {
+    try {
+        const collegeName = decodeURIComponent(req.params.name);
+        const members = await User.find({ collegeName }).select("-password -email");
+
+        res.status(200).json({
+            name: collegeName,
+            memberCount: members.length,
+            members,
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const getUsersByCompany = async (req, res) => {
+    try {
+        const companyName = decodeURIComponent(req.params.name);
+        const members = await User.find({ companyName }).select("-password -email");
+
+        res.status(200).json({
+            name: companyName,
+            memberCount: members.length,
+            members,
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
