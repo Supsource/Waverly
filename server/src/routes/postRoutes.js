79700fd7cec5f3bb19aa99afe80e0f09a26090d1
Supriyo <supriyomalstartup@gmail.com>
2026-05-30 18:23:21 +0530

    feat(server): add route for posts by username

--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { createPost, getAllPosts, getMyPosts, deletePost } from "../controllers/postController.js";
+import {
+    createPost,
+    getAllPosts,
+    getMyPosts,
+    getPostsByUsername,
+    deletePost,
+} from "../controllers/postController.js";
 const postRouter = express.Router();
 
 postRouter.post("/", protect, createPost);
 postRouter.get("/", protect, getAllPosts);
 postRouter.get("/me", protect, getMyPosts);
+postRouter.get("/user/:username", protect, getPostsByUsername);
 postRouter.delete("/:id", protect, deletePost);
 
 export default postRouter;
